0471dac3d20873803cd67354f108159d620f85c3
Bryan Shepherd <bsh******@***********resesarch.com>
2013-05-05 22:16:14 -0400

    Chapter1 1.0 - 5/5/2013

---
+++ Chapter1.R
@@ -0,0 +1,47 @@
+if(!suppressWarnings(require(igraph))) {
+  install.packages("igraph")
+  library(igraph)
+} else {
+  library(igraph)
+}
+
+#### Create the formal org chart
+
+# Create an edgelist to pass to the graph function 
+o.el <- matrix(c('Conrad','Mary',
+                 'Conrad','Cindy',
+                 'Conrad','Alice',
+                 'Alice','Brad',
+                 'Alice','Angie',
+                 'Alice','Jim',
+                 'Cindy','Samuel',
+                 'Cindy','Dave',
+                 'Cindy','Frida'), ncol=2, byrow=TRUE)
+
+o <- graph.edgelist(o.el, directed=FALSE)
+
+plot(o)
+             
+#### Create informal networks chart
+
+a.el <- matrix(c('Cindy','Samuel',
+               'Cindy','Conrad',
+               'Samuel','Frida',
+               'Conrad','Frida',
+               'Alice','Frida',
+               'Angie','Frida',
+               'Dave','Frida',
+               'Mary','Frida',
+               'Brad','Mary'), ncol=2, byrow=TRUE)
+
+a <- graph.edgelist(a.el, directed=FALSE)
+
+?add.vertices
+
+a <- a + vertex("Jim")
+
+plot(a)
+          
+#### More information
+
+# http://igraph.sourceforge.net/igraphbook/igraphbook-creating.html
